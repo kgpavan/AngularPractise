00878e6ee762cced73b48de0b9573f637dba2a98
Roopa Sundari <sundarigv@gmail.com>
2018-06-24 10:26:18 +0100

    product-detail fixes

--- APM-Start/src/app/products/product-detail.component.ts
+++ APM-Start/src/app/products/product-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Iproduct } from './product';
 import {ActivatedRoute} from '@angular/router';
 import {Router} from '@angular/router';
+import { ProductService } from './product.service';
+
 @Component({
 //  selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
@@ -11,24 +13,37 @@ export class ProductDetailComponent implements OnInit {
 
   pageTitle: string = 'Product Detail';
   product: Iproduct;
-  constructor(private _route:ActivatedRoute, private _router: Router)
+  errorMessage: string;
+
+  constructor(private _route: ActivatedRoute,
+    private _router: Router,
+    private _productService: ProductService)
    { }
 
   ngOnInit() {
-    let id =+this._route.snapshot.paramMap.get('id');
-    this.pageTitle+= `${id}`;
-    this.product = 
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-  }
+    const param = this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
+      
+    }
+  //   this.product = 
+  //   {
+  //     "productId": 1,
+  //     "productName": "Leaf Rake",
+  //     "productCode": "GDN-0011",
+  //     "releaseDate": "March 19, 2016",
+  //     "description": "Leaf rake with 48-inch wooden handle.",
+  //     "price": 19.95,
+  //     "starRating": 3.2,
+  //     "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+  // }
 
+  }
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
   onBack(): void{
       this._router.navigate(['/products']);
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -4,6 +4,10 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+//import { Iroduct } from './product';
+import 'rxjs/add/operator/map';
+
+
 @Injectable()
 export class ProductService{
     private _productUrl = './api/products/products.json';
@@ -14,6 +18,10 @@ export class ProductService{
         return this._http.get<Iproduct[]>(this._productUrl)
         .do(data => console.log('All: '+ JSON.stringify(data)))
         .catch(this.handleError);
+    }
+    getProduct(id: number): Observable<Iproduct> {
+        return this.getProducts()
+            .map((products: Iproduct[]) => products.find(p => p.productId === id));
     }
     private handleError(err: HttpErrorResponse){
         console.log(err.message);
